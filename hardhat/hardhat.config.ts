42ee89a97948da21e7b6fe92ffa7ae6e9b9a563a
Wealthineer <hi@wealthineer.com>
2023-08-12 10:35:32 +0200

    Added hardhat setup for Polygon zkEVM

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -8,8 +8,9 @@ import 'hardhat-contract-sizer';
 import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
-const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "please provide etherscan key...";
-const POLYGONSCAN = process.env.POLYGONSCAN_API_KEY || "please provide etherscan key...";
+const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "please provide etherscan api key...";
+const POLYGONSCAN = process.env.POLYGONSCAN_API_KEY || "please provide polygonscan api key...";
+const PLOyGONSCAN_ZKEVM = process.env.POLYGONSCAN_ZKEVM_API_KEY || "please provide polygonscan zkevm api key...";
 
 const GOERLI = `https://goerli.infura.io/v3/${process.env.INFURA}`;
 // const MAINNET = `https://mainnet.infura.io/v3/${process.env.INFURA}`;
@@ -18,6 +19,7 @@ const GOERLI = `https://goerli.infura.io/v3/${process.env.INFURA}`;
 const MAINNET = process.env.RPC_URL_MAINNET || "please provide rpc url mainnet...";
 const SEPOLIA = process.env.RPC_URL_SEPOLIA || "please provide rpc url sepolia...";
 const MUMBAI = process.env.RPC_URL_MUMBAI || "please provide rpc url mumbai...";
+const POLYGON_ZK_TEST= process.env.RPC_URL_POLYGON_ZK_TEST || "please provide rpc url mumbai...";
 
 
 
@@ -52,8 +54,14 @@ const config: HardhatUserConfig = {
       url: MUMBAI,
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
       chainId: 80001,
+    },
+    polygonZKEVMTestnet: {
+      url: POLYGON_ZK_TEST,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 1442,
     }
   },
+  
 
   typechain: {
     outDir: "typechain-types",
@@ -71,8 +79,20 @@ const config: HardhatUserConfig = {
       goerli: ETHERSCAN,
       sepolia: ETHERSCAN,
       polygon: POLYGONSCAN,
-      polygonMumbai: POLYGONSCAN
-    }
+      polygonMumbai: POLYGONSCAN,
+      polygonZKEVMTestnet: PLOyGONSCAN_ZKEVM,
+      popolygonZKEVMMainnet: PLOyGONSCAN_ZKEVM
+    },
+    customChains: [
+      {
+        network: "polygonZKEVMTestnet",
+        chainId: 1442,
+        urls: {
+          apiURL: "https://api-testnet-zkevm.polygonscan.com/api",
+          browserURL: "https://testnet-zkevm.polygonscan.com/"
+        }
+      }
+    ],
   },
 
   solidity: {
